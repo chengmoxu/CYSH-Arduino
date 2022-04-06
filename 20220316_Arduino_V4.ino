54d6081c577f36d08d7dea9b79664fc29a90d09c
Cheng-Mo, Hsu <99020504+Cheng-MoHsu@users.noreply.github.com>
2022-04-06 11:53:09 +0800

    Rename Arduino_20220316_V4.ino to 20220316_Arduino_V4.ino

---
+++ 20220316_Arduino_V4.ino
@@ -0,0 +1,37 @@
+void setup()
+{
+Serial.begin(9600);
+pinMode(6,OUTPUT);//馬達
+pinMode(5,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+}
+
+void loop() {
+  int a=3,b=4,c=5,d=6,e,f;
+  for(int i=0;i<100;i+=0)
+  {
+    digitalWrite(a,HIGH);
+    digitalWrite(b,LOW);
+    digitalWrite(c,HIGH);
+    digitalWrite(d,LOW);
+    delay(300);
+    e=a;
+    a=b;
+    b=e;
+    f=c;
+    c=d;
+    d=f;
+  }
+/*digitalWrite(6,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(4,LOW);
+digitalWrite(3,HIGH);
+delay(500);*/
+
+analogWrite(10,200);
+analogWrite(11,200);
+Serial.print("GO");
+}
